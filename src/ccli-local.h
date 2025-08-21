rostedt/libccli

commit 406290d26f7247bf586a163678bd280cf9dad088

ccli: Free history in ccli_free()
We forgot to free the history in ccli_free().

Fixes: ab983c904a6b1 ("ccli: The initial commit")
Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

src/ccli.c
@@ -299,6 +299,10 @@ void ccli_free(struct ccli *ccli)
 	for (i = 0; i < ccli->nr_commands; i++)
 		free(ccli->commands[i].cmd);
 
+	for (i = 0; i < ccli->history_size; i++)
+		free(ccli->history[i]);
+	free(ccli->history);
+
 	free(ccli->commands);
 	free(ccli->temp_line);
 	free(ccli);

commit 23d2a6f93365ff857d7d1f515fc9d4199d417e87

ccli: Free history properly
The history is freed in ccli_free() by iterating over the ccli->history[]
array up until ccli->history_size. But history_size is the number of
histories added. It may be larger than the history[] array. That's because
when the array hits ccli->history_max, it starts to loop over the array and
not add more entries.

Add a history_free() helper function in the history.c code and have the
ccli.c call that and not worry about how to actually free it.

Fixes: 406290d2 ("ccli: Free history in ccli_free()")
Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

src/ccli-local.h
@@ -130,6 +130,7 @@ extern int history_add(struct ccli *ccli, const char *line);
 extern int history_up(struct ccli *ccli, struct line_buf *line, int cnt);
 extern int history_down(struct ccli *ccli, struct line_buf *line, int cnt);
 extern int history_search(struct ccli *ccli, struct line_buf *line, int *pad);
+extern void history_free(struct ccli *ccli);
 
 extern void free_argv(int argc, char **argv);
 

src/ccli.c
@@ -406,9 +406,7 @@ void ccli_free(struct ccli *ccli)
 	for (i = 0; i < ccli->nr_commands; i++)
 		free(ccli->commands[i].cmd);
 
-	for (i = 0; i < ccli->history_size; i++)
-		free(ccli->history[i]);
-	free(ccli->history);
+	history_free(ccli);
 
 	free(ccli->commands);
 	free(ccli->temp_line);

src/history.c
@@ -21,6 +21,20 @@ static inline int history_idx(struct ccli *ccli, int idx)
 	return idx % ccli->history_max;
 }
 
+__hidden void history_free(struct ccli *ccli)
+{
+	int max = ccli->history_size;
+
+	if (ccli->history_size > ccli->history_max)
+		max = ccli->history_max;
+
+	for (int i = 0; i < max; i++)
+		free(ccli->history[i]);
+
+	free(ccli->history);
+	ccli->history = NULL;
+}
+
 __hidden int history_add(struct ccli *ccli, const char *line)
 {
 	char **lines;